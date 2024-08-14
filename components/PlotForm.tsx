ed8815282106fd0028312b7daa0e6d4f00ac0d6f
Shehab Yasser <shehabyasser@gmail.com>
2024-08-14 07:59:58 -0400

    Modified PlotForm

--- components/PlotForm.tsx
+++ components/PlotForm.tsx
@@ -6,37 +6,87 @@ interface PlotFormProps {
 }
 
 const PlotForm: React.FC<PlotFormProps> = ({ onSubmit }) => {
-  const [xValues, setXValues] = useState<string>('');
-  const [yValues, setYValues] = useState<string>('');
+  const [x1, setX1] = useState<string>('');
+  const [y1, setY1] = useState<string>('');
+  const [x2, setX2] = useState<string>('');
+  const [y2, setY2] = useState<string>('');
+  const [x3, setX3] = useState<string>('');
+  const [y3, setY3] = useState<string>('');
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const xArray = xValues.split(',').map(Number);
-    const yArray = yValues.split(',').map(Number);
-    onSubmit(xArray, yArray);
+    const xValues = [parseFloat(x1), parseFloat(x2), parseFloat(x3)];
+    const yValues = [parseFloat(y1), parseFloat(y2), parseFloat(y3)];
+    onSubmit(xValues, yValues);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        X Values (comma-separated):
-        <input
-          type="text"
-          value={xValues}
-          onChange={(e) => setXValues(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Y Values (comma-separated):
-        <input
-          type="text"
-          value={yValues}
-          onChange={(e) => setYValues(e.target.value)}
-        />
-      </label>
-      <br />
-      <button type="submit">Generate Plot</button>
+    <form onSubmit={handleSubmit} className="p-3">
+      <h3>Strategic Life Units</h3>
+      <div className="form-group">
+        <h4>Point 1</h4>
+        <label className="mr-2">
+          X1:
+          <input
+            type="text"
+            value={x1}
+            onChange={(e) => setX1(e.target.value)}
+            className="form-control d-inline-block w-auto ml-2"
+          />
+        </label>
+        <label className="mr-2">
+          Y1:
+          <input
+            type="text"
+            value={y1}
+            onChange={(e) => setY1(e.target.value)}
+            className="form-control d-inline-block w-auto ml-2"
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <h4>Point 2</h4>
+        <label className="mr-2">
+          X2:
+          <input
+            type="text"
+            value={x2}
+            onChange={(e) => setX2(e.target.value)}
+            className="form-control d-inline-block w-auto ml-2"
+          />
+        </label>
+        <label className="mr-2">
+          Y2:
+          <input
+            type="text"
+            value={y2}
+            onChange={(e) => setY2(e.target.value)}
+            className="form-control d-inline-block w-auto ml-2"
+          />
+        </label>
+      </div>
+      <div className="form-group">
+        <h4>Point 3</h4>
+        <label className="mr-2">
+          X3:
+          <input
+            type="text"
+            value={x3}
+            onChange={(e) => setX3(e.target.value)}
+            className="form-control d-inline-block w-auto ml-2"
+          />
+        </label>
+        <label className="mr-2">
+          Y3:
+          <input
+            type="text"
+            value={y3}
+            onChange={(e) => setY3(e.target.value)}
+            className="form-control d-inline-block w-auto ml-2"
+          />
+        </label>
+      </div>
+      <button type="submit" className="btn btn-primary">Generate Plot</button>
     </form>
   );
 };
