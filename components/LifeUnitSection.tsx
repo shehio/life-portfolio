cf41d7f1f68a41ba0ac7cdd74e73411d9b12cdf2
Shehab Yasser <shehabyasser@gmail.com>
2024-08-15 19:54:29 -0400

    Modified LUS

--- components/LifeUnitSection.tsx
+++ components/LifeUnitSection.tsx
@@ -4,16 +4,20 @@ interface LifeUnitSectionProps {
   title: string;
   importanceValue: string;
   satisfactionValue: string;
+  hoursPerWeek: string;
   onImportanceChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
   onSatisfactionChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  onHoursChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const LifeUnitSection: React.FC<LifeUnitSectionProps> = ({
   title,
   importanceValue,
   satisfactionValue,
+  hoursPerWeek,
   onImportanceChange,
   onSatisfactionChange,
+  onHoursChange,
 }) => {
   // Create an array of numbers from 1 to 10 for the dropdown options
   const options = Array.from({ length: 10 }, (_, i) => (i + 1).toString());
@@ -52,6 +56,20 @@ const LifeUnitSection: React.FC<LifeUnitSectionProps> = ({
                 </option>
               ))}
             </select>
+          </label>
+        </div>
+        <div className="col-md-2">
+          <label className="form-label">
+            Hours per Week:
+            <input
+              type="number"
+              value={hoursPerWeek}
+              onChange={onHoursChange}
+              className="form-control"
+              min="0"
+              max="167"
+              step="1"
+            />
           </label>
         </div>
       </div>
